1ff6ce80fb02eb7ce7eeae29f04c946bb41d9af3
AlvaroHJ21 <alvarohuaysara@gmail.com>
2022-01-18 19:07:39 -0500

    fanout

--- src/main/java/FisiTiendaUtiles/Facturacion.java
+++ src/main/java/FisiTiendaUtiles/Facturacion.java
@@ -8,9 +8,9 @@
  * Vera Cueva Antonella Jazmin
 
  */
-
 package FisiTiendaUtiles;
 
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -21,14 +21,51 @@ import java.nio.charset.Charset;
     reserva_facturacion
     y tiene que enviar un mensaje a CuentasCobrar a través de la cola 
     facturacion_cuentas
-*/
-
+ */
 public class Facturacion {
-    
-    private static final String NAME_QUEUE_1 = "";
-    private static final String NAME_QUEUE_2 = "";
-    
-    public static void main(String[] args) {
-        
+
+    private static final String NAME_QUEUE_RECEIVE = "";
+    private static final String NAME_QUEUE_SEND_1 = Queues.NAME_QUEUE_FACTURACION_CUENTAS;
+    private static final String NAME_QUEUE_SEND_2 = Queues.NAME_QUEUE_FACTURACION_ORDENES;
+    private static final String NAME_EXCHANGE = "facturacion";
+
+    public static void main(String[] args) throws Exception {
+
+        //envia el mensaje
+        enviarMensaje("mensaje enviado por un exchange fanout");
+
+    }
+
+    private static void enviarMensaje(String mensaje) {
+        try {
+            
+            //Abrir un conexion AMQ
+            ConnectionFactory conectionFactory = new ConnectionFactory();
+            Connection connection = conectionFactory.newConnection();
+            
+            //establecer un canal
+            Channel channel = connection.createChannel();
+
+            //Declarar un exchange FANOUT
+            channel.exchangeDeclare(NAME_EXCHANGE, BuiltinExchangeType.FANOUT);
+
+            //Declarar la colas
+            channel.queueDeclare(NAME_QUEUE_SEND_1, false, false, false, null);
+            channel.queueDeclare(NAME_QUEUE_SEND_2, false, false, false, null);
+
+            //Vincula las colas al exchange
+            channel.queueBind(NAME_QUEUE_SEND_1, NAME_EXCHANGE, "");
+            channel.queueBind(NAME_QUEUE_SEND_2, NAME_EXCHANGE, "");
+
+            //Enviar mensaje al exchange nuevo
+            channel.basicPublish(NAME_EXCHANGE, "", null, mensaje.getBytes());
+            System.out.println("Mensaje enviado: " + mensaje);
+
+            channel.close();
+            connection.close();
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
